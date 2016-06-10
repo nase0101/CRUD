3ac6946611d168eb332b5fc5f52a75aef077e391
Jim <jim@ChendeMacBook-Pro.local>
2016-06-10 14:18:49 +0800

    bootstrap ul, li

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,14 @@
 class EventsController < ApplicationController
   def index
     @events = Event.all
+    #@events = Event.page(params[:page]).per(5)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml { render :xml => @events.to_xml }
+      format.json { render :json => @events.to_json }
+      format.atom { @feed_title = "My event list" } # index.atom.builder
+    end
   end
 
   def new
